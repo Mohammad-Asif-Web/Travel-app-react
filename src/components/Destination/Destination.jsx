e4ac272b05fa0af6db7c02e14db7bb8a105a1f45
Muhammad Asif <alexasifhhossain@gmail.com>
2023-06-08 18:45:31 +0600

    destination uncompleted

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -8,7 +8,19 @@ function Destination() {
         <h1 className='text-center'>Destination Page</h1>
         <p className='text-center'>Tours give you the opportunity to live</p>
 
-        <DestinationData />
+        <DestinationData 
+          heading="Taal Volcano, Batangas"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto, nobis unde incidunt nisi cumque repellendus, blanditiis veniam magni iste dolore labore. Ex necessitatibus facilis voluptas sit, pariatur quae itaque officia aliquid molestias distinctio eligendi sed perferendis veritatis, non doloremque exercitationem? Temporibus vero id, ullam consectetur dolorum similique quam officia?"
+          img1="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+          img2="https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+        />
+
+        <DestinationData 
+          heading="Mt. Daguldul, Batangas"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto, nobis unde incidunt nisi cumque repellendus, blanditiis veniam magni iste dolore labore. Ex necessitatibus facilis voluptas sit, pariatur quae itaque officia aliquid molestias distinctio eligendi sed perferendis veritatis, non doloremque exercitationem? Temporibus vero id, ullam consectetur dolorum similique quam officia?"
+          img1="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+          img2="https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+        />
 
     </div>
   )
--- src/components/Destination/DestinationData.jsx
+++ src/components/Destination/DestinationData.jsx
@@ -6,12 +6,12 @@ class DestinationData extends Component {
     return (
         <div className="first-des">
             <div className="des-text">
-                <h2>Taal Volcano, Batangas</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto, nobis unde incidunt nisi cumque repellendus, blanditiis veniam magni iste dolore labore. Ex necessitatibus facilis voluptas sit, pariatur quae itaque officia aliquid molestias distinctio eligendi sed perferendis veritatis, non doloremque exercitationem? Temporibus vero id, ullam consectetur dolorum similique quam officia?</p>
+                <h2>{this.props.heading}</h2>
+                <p>{this.props.text}</p>
             </div>
             <div className="image">
-                <img src="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="des" />
-                <img src="https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="des" />
+                <img src={this.props.img1} alt="des_img1" />
+                <img src={this.props.img2} alt="des_img2" />
             </div>
         </div>
     )
