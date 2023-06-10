d67a44fee948fc703e20c5f772cd9a4fcd695994
Muhammad Asif <alexasifhhossain@gmail.com>
2023-06-10 11:41:35 +0600

    destination page dynamic on class, data style

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -9,6 +9,7 @@ function Destination() {
         <p className='text-center'>Tours give you the opportunity to live</p>
 
         <DestinationData 
+          classname="first-des"
           heading="Taal Volcano, Batangas"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto, nobis unde incidunt nisi cumque repellendus, blanditiis veniam magni iste dolore labore. Ex necessitatibus facilis voluptas sit, pariatur quae itaque officia aliquid molestias distinctio eligendi sed perferendis veritatis, non doloremque exercitationem? Temporibus vero id, ullam consectetur dolorum similique quam officia?"
           img1="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
@@ -16,10 +17,11 @@ function Destination() {
         />
 
         <DestinationData 
+          classname="first-des-reverse"
           heading="Mt. Daguldul, Batangas"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa architecto, nobis unde incidunt nisi cumque repellendus, blanditiis veniam magni iste dolore labore. Ex necessitatibus facilis voluptas sit, pariatur quae itaque officia aliquid molestias distinctio eligendi sed perferendis veritatis, non doloremque exercitationem? Temporibus vero id, ullam consectetur dolorum similique quam officia?"
-          img1="https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-          img2="https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+          img1="https://images.unsplash.com/photo-1439393161192-32360eb753f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+          img2="https://plus.unsplash.com/premium_photo-1670055126633-f409ab387cb9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80"
         />
 
     </div>
--- src/components/Destination/DestinationData.jsx
+++ src/components/Destination/DestinationData.jsx
@@ -4,7 +4,7 @@ import "./DestinationStyle.css"
 class DestinationData extends Component {
   render() {
     return (
-        <div className="first-des">
+        <div className={this.props.classname}>
             <div className="des-text">
                 <h2>{this.props.heading}</h2>
                 <p>{this.props.text}</p>
