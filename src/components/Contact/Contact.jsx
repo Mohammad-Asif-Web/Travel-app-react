fb53ab927cfdbc88d89a6191b05e307d9d8a56c2
Muhammad Asif <alexasifhhossain@gmail.com>
2023-06-10 17:48:04 +0600

    contact completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./ContactStyle.css"
 import Navbar from '../Navbar';
 import Hero from '../Hero/Hero';
+import Footer from '../Footer/Footer';
 
  function Contact() {
   return (
@@ -13,6 +14,17 @@ import Hero from '../Hero/Hero';
           title="Get In Touch"
           url="/"
         />
+        <div className="contact-form">
+          <h2>Send a message to us!</h2>
+          <form>
+            <input type="text" placeholder='name' />
+            <input type="text" placeholder='email' />
+            <input type="text" placeholder='subject' />
+            <textarea placeholder='message' rows="4"></textarea>
+            <button>Send Message</button>
+          </form>
+        </div>
+        <Footer />
     </div>
   )
 }
