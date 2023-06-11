ba82e83f70cbf630ad14bf4b3d0659bcca0b7311
Muhammad Asif <alexasifhhossain@gmail.com>
2023-06-11 12:53:32 +0600

    RouterProvider added by custom routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App.jsx'
 import './index.css'
+import routes from "./Routes/Routes"
+import { RouterProvider } from "react-router-dom";
 
 // const todo = "Custom JS, CSS add";
 // const date = new Date();
@@ -13,9 +15,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <RouterProvider router={routes} />
     
   </React.StrictMode>,
 )
